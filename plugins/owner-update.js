5a978c5876f15608d811433e50a39a590ca4ddfa
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-06 09:59:33 -0500

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,13 +1,16 @@
 import { execSync } from 'child_process'
+
 let handler = async (m, { conn, text }) => {
-if (global.conn.user.jid == conn.user.jid) {
-let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-//require('fs').readdirSync('plugins').map(v=>global.reload('', v))
-conn.reply(m.chat, stdout.toString(), m)
-}}
-handler.help = ['update']
+ if (global.conn.user.jid == conn.user.jid) {
+ let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
+ conn.reply(m.chat, stdout.toString(), m)
+ }
+}
+
+handler.help = ['actualizar']
 handler.tags = ['owner']
-handler.command = ['update', 'actualizar'] 
+handler.command = /^(update|actualizar|fix|gitpull)$/i
+
 handler.rowner = true
 
 export default handler
