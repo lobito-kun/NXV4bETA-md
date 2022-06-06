fb5bbff43356232700d90b187d6eb7e8dab4f553
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-06 14:38:47 -0500

    Update random-anime.js

--- plugins/random-anime.js
+++ plugins/random-anime.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
- await conn.reply(m.chat, args[0], m) //global.wait
+ await conn.reply(m.chat, command, m) //global.wait
 
 let type = (args[0] || m.text.replace(usedPrefix, '')).toLowerCase()
 
