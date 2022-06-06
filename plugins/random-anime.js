a6b777cfd65116993c42f1fc4c2a9dc3d9f0f4c4
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-06 14:52:03 -0500

    Update random-anime.js

--- plugins/random-anime.js
+++ plugins/random-anime.js
@@ -8,33 +8,41 @@ let type = (command).toLowerCase()
 
 switch (type) {
 case 'waifu':
- let res = await fetch('https://api.waifu.pics/sfw/waifu')
- if (!res.ok) throw await res.text()
- let json = await res.json()
- if (!json.url) throw global.error
- conn.sendFile(m.chat, json.url, 'Error.jpg', '*WAIFU*', m)
+  let res = await fetch('https://api.waifu.pics/sfw/waifu')
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if (!json.url) throw global.error
+  conn.sendFile(m.chat, json.url, 'Error.jpg', '*WAIFU*', m)
 break
 
 case 'loli':
- let img = (await axios.get(`https://raw.githubusercontent.com/FG98F/team-fg/main/img/loli.json`)).data
- conn.sendFile(m.chat, pickRandom(img), 'Error.jpg', '*LOLI*', m)
+  let img = (await axios.get(`https://raw.githubusercontent.com/FG98F/team-fg/main/img/loli.json`)).data
+  conn.sendFile(m.chat, pickRandom(img), 'Error.jpg', '*LOLI*', m)
 break
 
 case 'neko':
-let res = await fetch('https://api.waifu.pics/sfw/neko')
+  let res = await fetch('https://api.waifu.pics/sfw/neko')
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.url) throw global.error
   conn.sendFile(m.chat, json.url, 'Error.jpg', '*NEKO*', m)
 break 
 
+case 'megumin':
+  let res = await fetch('https://api.waifu.pics/sfw/megumin')
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if (!json.url) throw global.error
+  conn.sendFile(m.chat, json.url, 'Error.jpg', '*MEGUMIN*', m)
+break
+
 default:
  }
 }
 
-handler.help = ['waifu', 'loli', 'neko']
+handler.help = ['waifu', 'loli', 'neko', 'megumin']
 handler.tags = ['random']
-handler.command = /^(waifu|loli|neko)$/i
+handler.command = /^(waifu|loli|neko|megumin)$/i
 
 export default handler
 
