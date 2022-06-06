1867a0e18dae399af65835fa4cab5c75cd734aaf
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-06 11:19:19 -0500

    Create togif.js

---
+++ plugins/togif.js
@@ -0,0 +1,19 @@
+let { webp2mp4 } = require('../lib/webp2mp4')
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+    if (!m.quoted) throw `balas stiker dengan perintah *${usedPrefix + command}*`
+    let mime = m.quoted.mimetype || ''
+    if (!/webp/.test(mime)) throw `balas stiker dengan perintah *${usedPrefix + command}*`
+    let media = await m.quoted.download()
+    let out = Buffer.alloc(0)
+    if (/webp/.test(mime)) {
+        out = await webp2mp4(media)
+    }
+    await conn.sendMessage(m.chat, {  video: out, caption: "*Se convirtió con éxito a GIF*", gifPlayback: true })
+}
+
+handler.help = ['togif']
+handler.tags = ['sticker']
+handler.command = /^(togif|agif)$/i
+
+module.exports = handler
