356ea3f7537fd02b46d81515f833e6d0b48e338d
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-06 14:10:10 -0500

    Update and rename anime-waifu.js to waifu.js

---
+++ plugins/waifu.js
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+ let res = await fetch('https://api.waifu.pics/sfw/waifu')
+ if (!res.ok) throw await res.text()
+ let json = await res.json()
+ if (!json.url) throw '❎ Error!'
+ conn.sendFile(m.chat, json, 'error.png', '*LOLI*', m)
+}
+
+handler.help = ['waifu']
+handler.tags = ['nime']
+handler.command = /^(waifu)$/i
+
+export default handler
