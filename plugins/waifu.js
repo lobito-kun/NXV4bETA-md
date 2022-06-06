578f099c8a41b21313d861f1abd0162a352d9e9f
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-06 14:19:18 -0500

    Update waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
  if (!res.ok) throw await res.text()
  let json = await res.json()
  if (!json.url) throw global.error
- conn.sendFile(m.chat, json, 'error.png', '*WAIFU*', m)
+ conn.sendFile(m.chat, json, 'error.png', '*WAIFU*', m).catch((e)=>{ m.reply(global.error) })
 }
 
 handler.help = ['waifu']
