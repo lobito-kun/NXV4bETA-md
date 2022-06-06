fd2c10df40273d631ce59d03d9294b03a88f3eb8
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-06 15:30:03 -0500

    Update tomp3.js

--- plugins/tomp3.js
+++ plugins/tomp3.js
@@ -4,6 +4,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
   if (!/video|audio/.test(mime)) throw `🏷️ Etiqueta un video con el comando *${usedPrefix + command}* para convertirlo a mp3`
+  await conn.reply(m.chat, global.wait, m)
   let media = await q.download?.()
   if (!media) throw 'Ocurrio un error al descargar el video'
   let audio = await toAudio(media, 'mp4')
