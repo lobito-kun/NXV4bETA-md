ded4dc8c4b4b15570c176fade5ecd0c074c8b03a
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-06 16:07:18 -0500

    Update and rename sticker-getexif.js to getexif.js

---
+++ plugins/getexif.js
@@ -0,0 +1,18 @@
+import { format } from 'util'
+const { default: { Image } } = await import('node-webpmux')
+
+let handler = async (m) => {
+  if (!m.quoted) return m.reply(`🏷️ Etiqueta un sticker con el comando *${usedPrefix + command}*`)
+  if (/sticker/.test(m.quoted.mtype)) {
+      let img = new Image()
+      await img.load(await m.quoted.download())
+      let ins = format(JSON.parse(img.exif.slice(22).toString()))
+      m.reply(ins)
+ }
+}
+
+handler.help = ['getexif']
+handler.tags = ['sticker']
+handler.command = /^(getexif|infosticker|infostick)$/i
+
+export default handler
