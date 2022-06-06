a3fb8bc13f04b8f83fc42230eb4eddadf5a982bf
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-06 15:26:57 -0500

    Update and rename audio-tomp3.js to tomp3.js

---
+++ plugins/tomp3.js
@@ -0,0 +1,18 @@
+import { toAudio } from '../lib/converter.js'
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+  let q = m.quoted ? m.quoted : m
+  let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
+  if (!/video|audio/.test(mime)) throw `🏷️ Etiqueta un video con el comando *${usedPrefix + command}* para convertirlo a mp3`
+  let media = await q.download?.()
+  if (!media) throw 'Ocurrio un error al descargar el video'
+  let audio = await toAudio(media, 'mp4')
+  if (!audio.data) throw 'Ocurrio un error al convertir a mp3'
+  conn.sendFile(m.chat, audio.data, 'Error.mp3', '', m, null, { mimetype: 'audio/mp4' })
+}
+
+handler.help = ['tomp3']
+handler.tags = ['fun']
+handler.command = /^(tomp3|toaudio|aaudio|amp3|)$/i
+
+export default handler
