ce1727cc93873ed9c2bc3659828cb4f6eed128ce
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-06 14:30:52 -0500

    Update waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -2,11 +2,20 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
  await conn.reply(m.chat, global.wait, m)
+
+let type = (args[0] || m.text.replace(usedPrefix, '')).toLowerCase()
+
+switch (type) {
+case 'waifu':
  let res = await fetch('https://api.waifu.pics/sfw/waifu')
  if (!res.ok) throw await res.text()
  let json = await res.json()
  if (!json.url) throw global.error
  conn.sendFile(m.chat, json.url, 'Error.jpg', '*WAIFU*', m)
+break
+
+default:
+ }
 }
 
 handler.help = ['waifu']
