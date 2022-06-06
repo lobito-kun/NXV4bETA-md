6b322d5122b0c9409a3f86070d7730a58bfcef36
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-06 11:25:32 -0500

    Update togif.js

--- plugins/togif.js
+++ plugins/togif.js
@@ -1,3 +1,35 @@
+/*import { spawn } from 'child_process'
+import { format } from 'util'
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+    if (!global.support.convert && !global.support.magick && !global.support.gm) return handler.disabled = true // Disable if doesnt support
+    const isSticker = `🏷️ Etiqueta un sticker con el comando *${usedPrefix + command}* para convertirlo a imagen`
+    if (!m.quoted) throw isSticker
+    let q = m.quoted
+    if (/sticker/.test(q.mediaType)) {
+        let sticker = await q.download()
+        if (!sticker) throw sticker
+        let bufs = []
+        const [_spawnprocess, ..._spawnargs] = [...(global.support.gm ? ['gm'] : global.support.magick ? ['magick'] : []), 'convert', 'webp:-', 'png:-']
+        let im = spawn(_spawnprocess, _spawnargs)
+        im.on('error', e => m.reply(format(e)))
+        im.stdout.on('data', chunk => bufs.push(chunk))
+        im.stdin.write(sticker)
+        im.stdin.end()
+        im.on('exit', () => {
+            conn.sendFile(m.chat, Buffer.concat(bufs), 'image.png', '*Se convirtió con éxito a Imagen*', m)
+        })
+    } else throw isSticker
+}
+
+handler.help = ['toimg']
+handler.tags = ['sticker']
+handler.command = /^(toimg|aimg|toimagen|aimagen)$/i
+
+export default handler
+
+
+
 import { webp2mp4 } from '../lib/webp2mp4'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
@@ -16,4 +48,4 @@ handler.help = ['togif']
 handler.tags = ['sticker']
 handler.command = /^(togif|agif)$/i
 
-export default handler
+export default handler*/
