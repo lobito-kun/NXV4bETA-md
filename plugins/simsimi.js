82ada7779da9a25f1a9bebf9255f250521700030
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-06 22:48:51 -0500

    Update and rename fun-simsimi.js to simsimi.js

---
+++ plugins/simsimi.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { text, usedPrefix, command }) => {
+  let name = conn.getName(m.sender)
+  if (!text) throw 'Ingrese un texto junto al comando'
+  try {
+  let res = await fetch(`https://api-sv2.simsimi.net/v2/?text=${text}&lc=es`)
+  let json = await res.json()
+  let tes = json.success
+   m.reply(`${tes}`) 
+  } catch {
+   m.reply('😔 No entiendo')
+  }
+}
+
+handler.help = ['simi']
+handler.tags = ['main']
+handler.command = /^(simi|simsimi)$/i
+
+export default handler
