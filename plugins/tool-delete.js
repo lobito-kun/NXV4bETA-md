2c8fee853b9240963e4de293b50752105a392795
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-06 19:17:54 -0500

    Update tool-delete.js

--- plugins/tool-delete.js
+++ plugins/tool-delete.js
@@ -1,13 +1,15 @@
 let handler = function (m) {
     if (!m.quoted) throw false
     let { chat, fromMe, isBaileys } = m.quoted
-    if (!fromMe) throw false
-    if (!isBaileys) throw '✳️ *Ese no es mi mensaje*'
+    if (!fromMe) throw 'Etiqueta un mensaje mio para eliminarlo'
+    if (!isBaileys) throw 'Ese no es mi mensaje'
     conn.sendMessage(chat, { delete: m.quoted.vM.key })
 }
+
 handler.help = ['delete']
 handler.tags = ['main']
-handler.command = ['del', 'delete'] 
-handler.admin = true 
+handler.command = /^(del|delete)$/i
+
+handler.admin = true
 
 export default handler
