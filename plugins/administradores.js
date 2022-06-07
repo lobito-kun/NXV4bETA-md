150bbd89a5295642e103178306b2c97d33b6a800
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-07 08:39:21 -0500

    Update administradores.js

--- plugins/administradores.js
+++ plugins/administradores.js
@@ -3,7 +3,7 @@ const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './
 const groupAdmins = participants.filter(p => p.admin)
 const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
-let text = `\t*‧ 🍭 Lista de Administradores\'as 🍭 ‧*
+let text = `\t*‧ 🍭 Lista de administradores\'as 🍭 ‧*
 
 *• Grupo:* ${groupMetadata.subject}
 *• Admins:*
