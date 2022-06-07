a8c7d23430634c1ffb65b5dfc524b48d23430e09
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-07 08:37:17 -0500

    Update and rename gp-staff.js to administradores.js

--- plugins/administradores.js
+++ plugins/administradores.js
@@ -1,20 +1,21 @@
 let handler = async (m, { conn, participants, groupMetadata, args }) => {
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/avatar_contact.png'
 const groupAdmins = participants.filter(p => p.admin)
-const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n▢ ')
+const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
+let text = `\t\t\t‧ 🍭 Lista de Administradores\'as 🍭 ‧
 
-let text = `
-≡ *STAFF DEL GRUPO* _${groupMetadata.subject}_
-
-┌─⊷ *ADMINS*
-▢ ${listAdmin}
-└───────────
-`.trim()
+*• Grupo:* ${groupMetadata.subject}
+*• Admins:*
+${listAdmin}
+`
 conn.sendFile(m.chat, pp, 'staff.png', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
 }
-handler.help = ['staff']
+
+handler.help = ['admins']
 handler.tags = ['group']
-handler.command = ['staff', 'admins', 'listadmin'] 
+handler.command = /^(adms|staff|admins|listadmin|administradores)$/i
+
 handler.group = true
+
 export default handler
