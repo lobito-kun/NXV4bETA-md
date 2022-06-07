64d183110197c0edc779197ab6f913aebb70d83b
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-06 21:56:40 -0500

    Update reportar-bug.js

--- plugins/reportar-bug.js
+++ plugins/reportar-bug.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw 'Ingrese el error ue desea reportar'
     if (text.length < 10) throw 'Especifique bien el error, mínimo 10 caracteres'
     if (text.length > 1000) throw 'Máximo 1000 caracteres para enviar el error'
-    let teks = `*[ REPORTE DE BUG O ERROR ]*\n\n*• Usuario:* @${m.sender.split`@`[0]}\n*• Texto:* ${text}`
+    let teks = `*[ REPORTE ]*\n\n*• Usuario:* @${m.sender.split`@`[0]}\n*• Texto:* ${text}`
     await conn.reply(global.owner[0][0] + '@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, m, {
         contextInfo: {
             mentionedJid: [m.sender]
