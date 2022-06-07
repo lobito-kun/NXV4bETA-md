1d7e95b41e5b9d8bc14b134159ec120973acdfe3
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-07 07:52:43 -0500

    Update nsfw-cosplay.js

--- plugins/nsfw-cosplay.js
+++ plugins/nsfw-cosplay.js
@@ -1,17 +1,19 @@
 
 import fetch from 'node-fetch'
 import axios from 'axios'
+
 let handler = async (m, { conn, command, usedPrefix }) => {
-if (!db.data.chats[m.chat].nsfw && m.isGroup) throw `❗El grupo no admite contenido nsfw \n\n Para habilitar escriba \n*${usedPrefix}enable* nsfw`
-var {age} = db.data.users[m.sender]
-   if (age <17) throw conn.reply(m.chat, '❎ Eres menor de edad! vuelve cuando tengas más de 18 años', m) 
-let cosp = (await axios.get(`https://raw.githubusercontent.com/FG98F/team-fg/main/img/pack.json`)).data 
-let url = cosp[Math.floor(Math.random() * cosp.length)]
-conn.sendButton(m.chat, `✅ Resultado 🤭`, `Vea más fotos aquí \nhttps://t.me/+8SKOTyja8rBlYTlh \n\n`, url, [['▷▷ Siguiente', `${usedPrefix + command}`]], m)
+  if (!db.data.chats[m.chat].nsfw && m.isGroup) throw `El grupo no admite contenido nsfw \n\n Para habilitar escriba \n*${usedPrefix}enable* nsfw`
+  var { age } = db.data.users[m.sender]
+  if (age <17) throw conn.reply(m.chat, 'Eres menor de edad! vuelve cuando tengas más de 18 años', m)
+  await conn.reply(m.chat, global.wait, m)
+  let cosp = (await axios.get(`https://raw.githubusercontent.com/FG98F/team-fg/main/img/pack.json`)).data 
+  let url = cosp[Math.floor(Math.random() * cosp.length)]
+  conn.sendFile(m.chat, json.result.nowatermark, 'Error.jpg', '*COSPLAY*', m)
 }
-handler.help = ['pack']
+
+handler.help = ['cosplay']
 handler.tags = ['nsfw']
-handler.command = ['pack', 'cosplay']
-handler.limit = true
-handler.register = true
+handler.command = /^(pack|cosplay)$/i
+
 export default handler
