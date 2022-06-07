a41de3143f0c6b7d8d64c2f6248883d5be6b7936
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-06 22:20:58 -0500

    Update tool-readmore.js

--- plugins/tool-readmore.js
+++ plugins/tool-readmore.js
@@ -1,14 +1,17 @@
 let handler = async (m, { conn, text }) => {
-    let [l, r] = text.split`|`
-    if (!l) l = ''
-    if (!r) r = ''
-    conn.reply(m.chat, l + readMore + r, m)
+  if (!text) throw 'Ingrese un texto junto al comando'
+  let [l, r] = text.split`|`
+  if (!l) l = ''
+  if (!r) r = ''
+  conn.reply(m.chat, l + readMore + r, m)
 }
-handler.help = ['readmore <text1>|<text2>'] 
+
+handler.help = ['leermas'] 
 handler.tags = ['tools']
-handler.command = ['leermas', 'readmore'] 
+handler.command = /^(leermas|readmore)$/i
 
 export default handler
+
 
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
