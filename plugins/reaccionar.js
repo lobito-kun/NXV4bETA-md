49e3f37361de2f45cb7c6cd2996d144b352a81b3
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-06 22:10:39 -0500

    Update and rename sticker-react.js to reaccionar.js

--- plugins/reaccionar.js
+++ plugins/reaccionar.js
@@ -1,12 +1,13 @@
 let { proto } = (await import('@adiwajshing/baileys')).default
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `✳️ Responde a un mensaje usando el comando más un emoji para reaccionar\n\n📌Ejemplo ${usedPrefix + command} 🤫`
+  if (!args[0]) throw 'Escribe un emoji junto al commando y etiqueta un mensaje'
   let q = m.quoted ? await m.getQuotedObj() : m
   conn.relayMessage(m.chat, { reactionMessage: proto.ReactionMessage.create({ key: q.key, text: args[0] }) }, { messageId: q.key.id })
 }
-handler.help = ['react <emoji>']
+
+handler.help = ['reaccionar']
 handler.tags = ['sticker']
-handler.command = ['react', 'reaccionar', 'reaccion'] 
+handler.command = /^(reaccionar|reaction|read|reaccion|reacción)$/i
 
 export default handler
