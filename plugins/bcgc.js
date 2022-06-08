91141752916cdedc38254384efd008828c0aa0e6
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-08 18:37:06 -0500

    Update bcgc.js

--- plugins/bcgc.js
+++ plugins/bcgc.js
@@ -1,12 +1,11 @@
-let handler  = async (m, { conn, text, usedPrefix, command }) => {
-let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us') && !v.read_only && v.message && !v.announce).map(v => v.jid)
-let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
-let teks = text ? text : cc.text
-if (!text) throw `uhm.. teksnya mana?\nContoh :\n\n${usedPrefix + command} Halo|Tampilkan Menu|#menu`
-let [t1, t2, t3] = text.split`|`
-conn.reply(m.chat, `_Mengirim pesan broadcast ke ${groups.length} grup_`, m)
-for (let id of groups) await conn.sendButton(id, `\t\t\t\t*Anuncio | grupos*\n\n${teks}`, '-', t2, t3)
-m.reply('Selesai Broadcast All Group 👍')
+import fs from 'fs'
+
+let handler = async (m, { conn, text } ) => {  
+  let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
+  let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
+  let teks = text ? text : cc.text
+  for (let id of groups) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bcgc|broadcastgroup/i.test(teks) ? teks : `\t\t\t\t*Anuncio | Grupos*\n\n${teks}` ), true).catch(_ => _)
+  m.reply(`El anuncio se envió a ${groups.length} grupos`)
 }
 
 handler.help = ['bcgc']
@@ -16,8 +15,3 @@ handler.command = /^(broadcastgroup|bcgc)$/i
 handler.owner = true
 
 export default handler
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
-
-const randomID = length => require('crypto').randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
