1a43a9e60570ccd9c88985fb1974e8c87576b8d8
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-08 18:02:03 -0500

    Update and rename owner-restart.js to reiniciar.js

---
+++ plugins/reiniciar.js
@@ -0,0 +1,17 @@
+import { spawn } from 'child_process'
+
+let handler = async (m, { conn, isROwner, text }) => {
+  if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
+  if (global.conn.user.jid == conn.user.jid) {
+  await m.reply('*🔄 Reiniciando la Bot . . .*\nEspere un momento')
+  process.send('reset')
+ } else throw 'eh'
+}
+
+handler.help = ['reiniciar']
+handler.tags = ['owner']
+handler.command = /^(restart|reiniciar)$/i
+
+handler.rowner = true
+
+export default handler
