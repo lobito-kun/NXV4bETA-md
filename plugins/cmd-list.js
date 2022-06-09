3d3bb2e4075c99d37e12e70d977b5e11ba6ebf5f
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-08 22:45:45 -0500

    Update cmd-list.js

--- plugins/cmd-list.js
+++ plugins/cmd-list.js
@@ -4,9 +4,9 @@ let handler = async (m, { conn }) => {
 *• Info:* Si esta en *negrita* esta bloqueado
 
 ${Object.entries(global.db.data.sticker).map(([key, value], index) => `*• ID:* ${value.locked ? `${key} (bloqueado)` : key}\n*• Cmd:* ${value.text}`).join('\n\n\n')}
-`, null, {
+`, m, null, {
         mentions: Object.values(global.db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])
-    }, m)
+    })
 }
 
 
