ffc759aedc47fe1a9685ef5c7746ef1a6efe3533
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-08 19:37:07 -0500

    Update nsfw-ass.js

--- plugins/nsfw-ass.js
+++ plugins/nsfw-ass.js
@@ -1,24 +1,43 @@
 import fetch from 'node-fetch'
+import axios from 'axios'
 
-let handler = async (m, { conn, usedPrefix, command }) => {
-	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw `❗El grupo no admite contenido nsfw \n\n Para habilitar escriba \n*${usedPrefix}enable* nsfw`
-  var {age} = db.data.users[m.sender]
-   if (age <17) throw conn.reply(m.chat, '❎ Eres menor de edad! vuelve cuando tengas más de 18 años', m) 
-   
-  let  pw = ["https://meme-api.herokuapp.com/gimme/CuteLittleButts", "https://meme-api.herokuapp.com/gimme/ass"]
-   let nk = pw[Math.floor(Math.random() * pw.length)] 
-    let res = await fetch(nk)
-    if (!res.ok) throw await res.text()
-    let json = await res.json()
-    if (!json.url) throw '❎ Error'
-    conn.sendButton(m.chat, '✅ Aquí tienes', igfg, json.url, [['▷▷ Siguiente', `${usedPrefix + command }`]], m)
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  var { age } = db.data.users[m.sender]
+  if (age < 17) throw conn.reply(m.chat, 'Eres menor de edad! vuelve cuando tengas más de 18 años', m) 
+  await conn.reply(m.chat, global.wait, m)
+
+let type = (command).toLowerCase()
+
+switch (type) {
+case 'ass':
+case 'culos':
+case 'culo':
+case 'nalgas':
+  let rass = ["https://meme-api.herokuapp.com/gimme/CuteLittleButts", "https://meme-api.herokuapp.com/gimme/ass"]
+  let nkass = rass[Math.floor(Math.random() * rass.length)] 
+  let resass = await fetch(nkass)
+  if (!resass.ok) throw await resass.text()
+  let jsonass = await resass.json()
+  if (!jsonass.url) throw global.error
+  conn.sendFile(m.chat, jsonass.url, 'Error.jpg', `${command.toUpperCase()}`, m)
+break
+
+
+
+default:
+ }
 }
+
 handler.help = ['ass']
 handler.tags = ['nsfw']
-handler.command = ['ass', 'culo', 'culos']
-handler.limit = true
+handler.command = /^(ass|culos|culo|nalgas)$/i
+handler.nsfw = true
 handler.register = true
 
-
-
 export default handler
+
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
