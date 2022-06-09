d78509839056981294bcc6d0f8186ddcd558eee0
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-08 21:56:43 -0500

    Update imagen.js

--- plugins/imagen.js
+++ plugins/imagen.js
@@ -1,5 +1,6 @@
 import { googleImage } from '@bochilteam/scraper'
-  let handler = async (m, { conn, text, usedPrefix, command }) => {
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) throw 'Que imagen quieres que busque?'
   const res = await googleImage(text)
   conn.sendFile(m.chat, res.getRandom(), 'image.jpg', `✅ Resultado de: *${text}*`, m)
