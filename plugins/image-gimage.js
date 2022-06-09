082b4be93ba26490f25286a3694cb245871087a7
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-08 21:54:22 -0500

    Update image-gimage.js

--- plugins/image-gimage.js
+++ plugins/image-gimage.js
@@ -1,14 +1,12 @@
 import { googleImage } from '@bochilteam/scraper'
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `✳️ Que imagen busco?`
-    const res = await googleImage(text)
-    conn.sendFile(m.chat, res.getRandom(), 'image.jpg', `
-✅ Resultado de : *${text}*
-`.trim(), m)
+  let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw 'Que imagen quieres que busque?'
+  const res = await googleImage(text)
+  conn.sendFile(m.chat, res.getRandom(), 'image.jpg', `✅ Resultado de: *${text}*`, m)
 }
+
 handler.help = ['imagen']
 handler.tags = ['img']
-handler.command = ['img', 'image', 'imagen'] 
-handler.limit = true 
+handler.command = /^(img|image|imagen)$/i
 
 export default handler
