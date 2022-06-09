b142b47e8476a61fd1e38f4f62457cd24a17d92e
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-08 21:57:24 -0500

    Create _comand-respond.js

---
+++ plugins/_comand-respond.js
@@ -0,0 +1,11 @@
+import { googleImage } from '@bochilteam/scraper'
+
+let handler = m => m
+
+handler.before = async function (m) {
+  if (!m.quoted || !m.quoted.fromMe || !m.text || !/^Que imagen quieres/i.test(m.quoted.text)) return false
+  const res = await googleImage(m.text)
+  conn.sendFile(m.chat, res.getRandom(), 'Error.jpg', `✅ Resultado de: *${text}*`, m)
+}
+
+export default handler
