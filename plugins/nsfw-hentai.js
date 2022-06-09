23037b42922b91345771a53117cbb51e01958484
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-08 19:21:49 -0500

    Update nsfw-hentai.js

--- plugins/nsfw-hentai.js
+++ plugins/nsfw-hentai.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-  var {age} = db.data.users[m.sender]
+  var { age } = db.data.users[m.sender]
   if (age <17) throw conn.reply(m.chat, 'Eres menor de edad! vuelve cuando tengas más de 18 años', m) 
   let pw = ["https://api.waifu.pics/nsfw/blowjob", "https://api.waifu.pics/nsfw/neko", "https://api.waifu.pics/nsfw/waifu"] 
   let nk = pw[Math.floor(Math.random() * pw.length)]
