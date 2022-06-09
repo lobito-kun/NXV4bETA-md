a7d6f32d68fa1aad3fba035814fd560de6d0875c
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-08 22:12:39 -0500

    Update _comand-respond.js

--- plugins/_comand-respond.js
+++ plugins/_comand-respond.js
@@ -4,6 +4,7 @@ let handler = m => m
 
 handler.before = async function (m) {
   if (!m.quoted || !m.quoted.fromMe || !m.text || !/^Que imagen quieres/i.test(m.quoted.text)) return false
+  await conn.reply(m.chat, global.wait, m)
   const res = await googleImage(m.text)
   conn.sendFile(m.chat, res.getRandom(), 'Error.jpg', `✅ Resultado de: *${m.text}*`, m)
 }
