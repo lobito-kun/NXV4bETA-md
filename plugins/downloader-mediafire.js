075647ec1403908a18b6ce6372a035c8076e6400
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-10 07:08:52 -0500

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -2,17 +2,16 @@ import { mediafiredl } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `✳️ Ingrese el link de mediafire junto al comando`
     if (!args[0].match(/mediafire/gi)) throw `❎ Link incorrecto`
-    
     let res = await mediafiredl(args[0])
     let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
-    /*let caption = `
+    let caption = `
    ≡ *MEDIAFIRE*
 
 ▢ *Nombre:* ${filename}
 ▢ *Tamaño:* ${filesizeH}
 ▢ *Extension:* ${ext}
 ▢ *Subido:* ${aploud}
-`.trim()*/
+`.trim()
     m.reply(global.wait)
     await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: 'application/vnd.android.package-archive', asDocument: true })
 }
