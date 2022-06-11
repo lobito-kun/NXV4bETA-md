fc46600c4af4f54757f9b01a337d2930f6b55614
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-11 13:32:59 -0500

    Update youtube-play.js

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -1,29 +1,27 @@
 import { youtubeSearch } from '@bochilteam/scraper'
+
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `✳️ *Ingresa el título de una canción*\n\n📌Ejemplo *${usedPrefix + command}* Lil Peep hate my life `
+  if (!text) throw 'Ingrese el nombre de un video de YouTube junto al comando'
   let vid = (await youtubeSearch(text)).video[0]
-  if (!vid) throw '✳️ Vídeo/Audio no encontrado'
+  if (!vid) throw 'Vídeo/Audio no encontrado'
   let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
   const url = 'https://www.youtube.com/watch?v=' + videoId
-  await conn.sendHydrated(m.chat, `
-  ≡ *FG MUSIC*
-┌──────────────
-▢ 📌 *Título* : ${title}
-▢ 📆 *Publicado:* ${publishedTime}
-▢ ⌚ *Duración:* ${durationH}
-▢ 👀 *Vistas:* ${viewH}
-└──────────────
-  `.trim(), igfg, thumbnail, fgyt, 'YouTube', null, null, [
-    ['🎶 MP3', `${usedPrefix}fgmp3 ${url} yes`],
-    ['🎥 MP4', `${usedPrefix}fgmp4 ${url} yes`]
+  let teksyt = `\t\t\t*× 📻 Descargɑ de YouTube 📻 ×*
+
+*• Titulo:* ${title}
+*• Duración:* ${durationH}
+*• Visitas:* ${viewH}
+*• Publicado:* ${publishedTime}
+*• Url:* ${_url}`
+  conn.sendButton(m.chat, teksyt.replace('a', 'ɑ').replace('á', 'ά'), 'elija un formato de descarga', thumbnail, [
+    ['Audio 🎶', `${usedPrefix}fgmp3 ${url} yes`],
+    ['Vídeo 🎥', `${usedPrefix}fgmp4 ${url} yes`]
   ], m)
 }
+
 handler.help = ['play']
 handler.tags = ['downloader']
-handler.command = ['play', 'playvid', 'play2'] 
-
-handler.exp = 0
-handler.limit = false
+handler.command = /^(play|playvid|play2)$/i
 
 export default handler
 
