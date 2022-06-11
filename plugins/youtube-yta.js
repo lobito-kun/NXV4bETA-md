66a80e470feb7f14bcd5198197f0a436cd42372d
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-11 13:40:22 -0500

    Update youtube-yta.js

--- plugins/youtube-yta.js
+++ plugins/youtube-yta.js
@@ -1,10 +1,11 @@
-let limit = 50
+let limit = 27
+
 import fetch from 'node-fetch'
 import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper';
+
 let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
-  if (!args || !args[0]) throw `✳️ Ejemplo :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`
- //m.reply('*⌛ _Cargando..._ ▬▬▬▭*')
- let chat = global.db.data.chats[m.chat]
+  if (!args || !args[0]) throw 'Ingrese el link de un video de YouTube junto al comando'
+  let chat = global.db.data.chats[m.chat]
   const isY = /y(es)/gi.test(args[1])
   const { thumbnail, audio: _audio, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
   const limitedSize = (isPrems || isOwner ? 350 : limit) * 3074
@@ -25,34 +26,16 @@ let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) =
       lastError = e
     }
   }
-  if ((!(source instanceof ArrayBuffer) || !link || !res.ok) && !isLimit) throw '❎ Error: ' + (lastError || 'no puedo descargar el audio')
+  if ((!(source instanceof ArrayBuffer) || !link || !res.ok) && !isLimit) throw '*Error:* ' + (lastError || 'no puedo descargar el audio')
  
- m.reply(isLimit ? `≡ *FG MUSIC* 
-  
-▢ *📌Titulo:* ${title}
-▢ *⚖️ Peso:* ${audio.fileSizeH}
-▢ *El archivo supera el límite de descarga*
-*Gratis :*
-${limit} mb
-▬▬▬▭▭ *300 MB*
-*Premium :*
-300 mb
-▬▬▬▬▬ *300 MB*`: global.wait) 
-  if (!isLimit) await conn.sendFile(m.chat, source, title + '.mp3', `
-≡  *FG MUSIC*
-    
-▢ *📌Título* : ${title}
-▢ *📟 Ext* : mp3
-▢ *⚖️Peso* : ${audio.fileSizeH}
-`.trim(), m, null, {
-    asDocument: chat.useDocument
-  })
+  m.reply(isLimit ? 'El archivo supera el límite de descarga' : global.wait) 
+  if (!isLimit) await conn.sendFile(m.chat, source, title + '.mp3', `*🍭 Título:* ${title}`, m, null, { asDocument: chat.useDocument })
 }
-handler.help = ['ytmp3 <link yt>']
+
+handler.help = ['ytmp3']
 handler.tags = ['downloader']
-handler.command = ['ytmp3', 'fgmp3'] 
+handler.command = /^(ytmp3|fgmp3)$/i
 
 handler.limit = true
-handler.exp = 0
 
 export default handler
