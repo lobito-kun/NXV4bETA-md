b3b2a26fee24df91cad342212078241f31f9db45
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-18 18:01:57 -0500

    Update bc.js

--- plugins/bc.js
+++ plugins/bc.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text }) => {
   let fakegif = { key: {participant: `0@s.whatsapp.net`, ...("6289643739077-1613049930@g.us" ? { remoteJid: "6289643739077-1613049930@g.us" } : {})},message: {"videoMessage": { "title": 'lolibot', "h": `Hmm`,'seconds': '99999', 'gifPlayback': 'true', 'caption': 'Broadcast 🐈', 'jpegThumbnail': false }}}
   let teks = `\t\t\t\t*Anuncio | grupos*\n\n${text}`
   for (let id of chats) {
-  await conn.sendMessage(m.chat, { text: teks }, { quoted: fakegif })
+  await conn.sendMessage(id, { text: teks }, { quoted: fakegif })
   }
   conn.reply(m.chat, `El anuncio se envío a *${groups.length} chats*!`, m)
 }
