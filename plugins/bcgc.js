4bc2dd9fc1d9f6b526071c5e6a0ba36ae732fd2f
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-18 17:54:06 -0500

    Update bcgc.js

--- plugins/bcgc.js
+++ plugins/bcgc.js
@@ -1,11 +1,14 @@
 import fs from 'fs'
 
-let handler = async (m, { conn, text } ) => {  
+let handler = async (m, { conn, text } ) => {
+  if (!text) throw 'Ingrese un texto para enviar el anuncio a todos los grupos'
   let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
-  let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
-  let teks = text ? text : cc.text
-  for (let id of groups) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bcgc|broadcastgroup/i.test(teks) ? teks : `\t\t\t\t*Anuncio | Grupos*\n\n${teks}` ), true).catch(_ => _)
-  m.reply(`El anuncio se envió a ${groups.length} grupos`)
+  let fakegif = { key: {participant: `0@s.whatsapp.net`, ...("6289643739077-1613049930@g.us" ? { remoteJid: "6289643739077-1613049930@g.us" } : {})},message: {"videoMessage": { "title": 'lolibot', "h": `Hmm`,'seconds': '99999', 'gifPlayback': 'true', 'caption': 'Broadcast 🐈', 'jpegThumbnail': false }}}
+  let teks = `\t\t\t\t*Anuncio | grupos*\n\n${text}`
+  for (let id of groups) {
+  await conn.sendMessage(m.chat, { text: teks, mentions: (await conn.groupMetadata(`${id}`)).participants.map(v => v.id) }, { quoted: fakegif })
+  }
+  conn.reply(m.chat, `El anuncio se envío a *${groups.length} grupos*!`, m)
 }
 
 handler.help = ['bcgc']
