ec2dc46fcac597b9dbe0360859d4a3adaa551d7a
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-19 00:39:27 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -4,6 +4,7 @@ import fetch from 'node-fetch'
 import fs from 'fs'
 
 let handler = async (m, { conn, text } ) => {
+  conn.reply(m.chat, '*Test*', false, { quoted: m, contextInfo: { externalAdReply: { showAdAttribution: true } }})
   conn.sendButton(m.chat, `*Test button*`, '-', imgmenu, [['Speedtest', '.ping'], ['Owner', '.owner']], false, { quoted: m, contextInfo: { externalAdReply: { showAdAttribution: true,
 mediaType: 'VIDEO',
 mediaUrl: 'https://www.kibrispdr.org/dwn/7/yotsuba-nakano-wallpaper.jpg',
