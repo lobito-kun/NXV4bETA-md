818c0a859afeebc4378140bef038a4eb4d579aeb
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-18 20:12:14 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -6,10 +6,12 @@ let handler = async (m, { conn, text } ) => {
   let url = 'https://wallpapercave.com/mwp/wp5580868.jpg'
   conn.sendButton(m.chat, `*Test button*`, '-', url, [['Owner', '.owner'],['Info', '.info']], false, { quoted: m, 
     contextInfo: { externalAdReply: {
-    title: 'Test',
+    title: 'Test', 
     body: '-', 
-    sourceUrl: '', 
-    thumbnail: false
+    mediaType: '2', 
+    previewType: 'VIDEO', 
+    thumbnail: false, 
+    mediaUrl: 'https://youtu.be/S8ivHgSumeg'
   }}})
 
   //conn.sendButton(m.chat, 'Test', '-', url, [['Info', `.info`], ['Owner', `.owner`]], m, {contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: '𝚁𝙴𝙿𝚁𝙾𝙳𝚄𝙲𝚃𝙾𝚁 𝙳𝙴 𝚅𝙸𝙳𝙴𝙾 𝚅𝟸',body: 'ʙʏ ᴛʜᴇ ᴍʏsᴛɪᴄ ﹣ ʙᴏᴛ', previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}})
