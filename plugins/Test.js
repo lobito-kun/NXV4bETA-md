bf912d5655768c1ab7c18a017afd109331ba9632
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-18 19:45:21 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -2,10 +2,9 @@ import MessageType from '@adiwajshing/baileys'
 import fs from 'fs'
 
 let handler = async (m, { conn, text } ) => {
-  let bb = await conn.prepareMessage('0@s.whatsapp.net', false, 'documentMessage', { mimetype: 'application/pdf', thumbnail: false })
-  let _p = '!'
-  let cc = await conn.prepareMessageFromContent(m.chat, { buttonsMessage: { contentText: `Hi! Im ${conn.user.name}\n\nHere my menu...`, footerText: 'Test', buttons: [{ buttonId: `${_p}ping`, buttonText: { displayText: `ping` }, type: 1 }, { buttonId: `${_p}owner`, buttonText: { displayText: `owner` }, type: 1 }, { buttonId: `${_p}donasi`, buttonText: { displayText: `donasi` }, type: 1 }], headerType: 'DOCUMENT', documentMessage: { url: bb.message.documentMessage.url, mimetype: bb.message.documentMessage.mimetype, title: bb.message.documentMessage.title, fileSha256: bb.message.documentMessage.fileSha256, fileLength: '999999999', pageCount: '100', mediaKey: bb.message.documentMessage.mediaKey, fileName: conn.user.name, fileEncSha256: bb.message.documentMessage.fileEncSha256, directPath: bb.message.documentMessage.directPath, jpegThumbnail: bb.message.documentMessage.jpegThumbnail }}}, { quoted: m, contextInfo: { externalAdReply: { title: 'ᴅᴀғʏ乂ɴᴇʀɪɴᴀ乂ᴠᴀʟᴇɴ', body: 'ɪɴɪ ʙᴜᴀᴛᴀɴ ᴅᴀғʏ', mediaType: 2, thumbnail: false, mediaUrl: 'https://instagram.com/raaihankhadafi8' }}})
-  conn.relayWAMessage(cc)
+  let girl = (await axios.get(`https://raw.githubusercontent.com/FG98F/team-fg/main/img/girl.json`)).data
+  let url = pickRandom(girl)
+  conn.sendButton(m.chat, 'Test', '-', url, [['Info', `.info`], ['Owner', `.owner`]], m)
 }
 
 handler.command = ['test']
