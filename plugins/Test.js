2fe23df5fc2e45e34da880713bbeb470e58d9a38
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-19 00:37:15 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -1,35 +1,21 @@
-/*import MessageType from '@adiwajshing/baileys'
+import MessageType from '@adiwajshing/baileys'
 import axios from 'axios'
 import fetch from 'node-fetch'
 import fs from 'fs'
 
 let handler = async (m, { conn, text } ) => {
-  let url = await(await fetch('https://wallpapercave.com/mwp/wp5580868.jpg')).buffer()
-  conn.sendButton(m.chat, `*Test button*`, '-', imgmenu, [['Owner', '.owner'],['Info', '.info']], false, { quoted: m, 
-    contextInfo: { externalAdReply: {
-    title: 'Test', 
-    body: '-', 
-    mediaType: '2', 
-    previewType: 'VIDEO', 
-    thumbnail: url, 
-    mediaUrl: 'https://youtu.be/S8ivHgSumeg'
-  }}})
-
-  await conn.reply(m.chat, `*Test url*`, m, {
-    contextInfo: { externalAdReply: {
-    title: 'Test', 
-    body: '-', 
-    mediaType: '2', 
-    previewType: 'VIDEO', 
-    thumbnail: url, 
-    mediaUrl: 'https://youtu.be/S8ivHgSumeg'
-  }}})
-
-  //conn.sendButton(m.chat, 'Test', '-', url, [['Info', `.info`], ['Owner', `.owner`]], m, {contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: '𝚁𝙴𝙿𝚁𝙾𝙳𝚄𝙲𝚃𝙾𝚁 𝙳𝙴 𝚅𝙸𝙳𝙴𝙾 𝚅𝟸',body: 'ʙʏ ᴛʜᴇ ᴍʏsᴛɪᴄ ﹣ ʙᴏᴛ', previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}})
+  conn.sendButton(m.chat, `*Test button*`, '-', imgmenu, [['Speedtest', '.ping'], ['Owner', '.owner']], false, { quoted: m, contextInfo: { externalAdReply: { showAdAttribution: true,
+mediaType: 'VIDEO',
+mediaUrl: 'https://www.kibrispdr.org/dwn/7/yotsuba-nakano-wallpaper.jpg',
+title: 'Simple MD',
+body: 'By Rendyzzx',
+thumbnail: imgmenu,
+sourceUrl: 'https://youtu.be/poD-7_U3jXk'
+}
+  }
+})
 }
 
 handler.command = ['test']
-handler.tags = ['owner']
-handler.help = /^(test)$/i
 
-export default handler*/
+export default handler
