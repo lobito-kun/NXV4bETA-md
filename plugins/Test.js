eb06b9539e5726455897f0c9d1a07187507c2362
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-18 20:05:48 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -4,7 +4,15 @@ import fs from 'fs'
 
 let handler = async (m, { conn, text } ) => {
   let url = 'https://wallpapercave.com/mwp/wp5580868.jpg'
-  conn.sendButton(m.chat, 'Test', '-', url, [['Info', `.info`], ['Owner', `.owner`]], m)
+  conn.sendButton(m.chat, `*Test button*`, '-', url, [['Owner', '.owner'],['Info', '.info']], m, false, { 
+    contextInfo: { externalAdReply: {
+    title: 'Test',
+    body: '-', 
+    sourceUrl: '', 
+    thumbnail: url 
+  }}})
+
+  //conn.sendButton(m.chat, 'Test', '-', url, [['Info', `.info`], ['Owner', `.owner`]], m, {contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: '𝚁𝙴𝙿𝚁𝙾𝙳𝚄𝙲𝚃𝙾𝚁 𝙳𝙴 𝚅𝙸𝙳𝙴𝙾 𝚅𝟸',body: 'ʙʏ ᴛʜᴇ ᴍʏsᴛɪᴄ ﹣ ʙᴏᴛ', previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}})
 }
 
 handler.command = ['test']
