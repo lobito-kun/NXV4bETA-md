f9f040c5e6e416144d69678ece08a962cd70e9a9
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-20 11:18:47 -0500

    Update leermas.js

--- plugins/leermas.js
+++ plugins/leermas.js
@@ -2,8 +2,8 @@ let handler = async (m, { conn, text }) => {
   if (!text) throw 'Ingrese un texto junto al comando'
   let [l, r] = text.split`|`
   if (!l) l = ''
-  if (!r) r = ' '
-  conn.reply(m.chat, l + readMore + r, m)
+  if (!r) r = ''
+  conn.reply(m.chat, l + readMore + ' ' + r, m)
 }
 
 handler.help = ['leermas'] 
