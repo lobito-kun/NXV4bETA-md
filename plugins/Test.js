ff2666473c4e87dfaf2dd2d538de86bd18b0527d
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-20 14:13:42 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -4,7 +4,9 @@ import fetch from 'node-fetch'
 import fs from 'fs'
 
 let handler = async (m, { conn, text } ) => {
-  conn.sendButton(m.chat, `*Test button*`, 'Creɑted by gɑtito ⾕', imgmenu, [['Info 🧃', '.ping'], ['Creador 🍭', '.owner']], false, { quoted: m, contextInfo: { externalAdReply: {
+  conn.sendButton(m.chat, `*Test button*`, '-', imgmenu, [['Speedtest', 'ping'], ['Owner', 'owner']], false, { quoted: m, contextInfo: { externalAdReply: { showAdAttribution: true, mediaType: 'VIDEO', mediaUrl: imgmenu, title: 'Simple Bot Esm', body: 'By Papah-Chan',thumbnail: imgmenu, sourceUrl: 'https://youtu.be/poD-7_U3jXk' } } })
+
+  /*conn.sendButton(m.chat, `*Test button*`, 'Creɑted by gɑtito ⾕', imgmenu, [['Info 🧃', '.ping'], ['Creador 🍭', '.owner']], false, { quoted: m, contextInfo: { externalAdReply: {
   showAdAttribution: true,
   mediaType: 'VIDEO',
   mediaUrl: '',
@@ -12,7 +14,7 @@ let handler = async (m, { conn, text } ) => {
   body: 'By gɑtito ⾕',
   thumbnail: miniurl,
   sourceUrl: 'https://chat.whatsapp.com/ELn6Ck7InoP6UmA3QiQsgo'
-  }}})
+  }}})*/
 }
 
 handler.command = ['test']
