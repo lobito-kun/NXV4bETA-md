c8f5628b26f54d82e2b71426ef366e79d00214f5
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-20 11:12:33 -0500

    Update emojimix.js

--- plugins/emojimix.js
+++ plugins/emojimix.js
@@ -4,7 +4,7 @@ import { sticker } from '../lib/sticker.js'
 import fs from 'fs'
 
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-  if (!args[0]) throw `*⛌ Ingrese los emojis que desea combinar*\n\n*• Ejemplo:*\n- !emojimix 🐱+👻\n\n[ mínimo 2 emojis ]`
+  if (!args[0]) throw `*⛌ Ingrese los emojis que desea combinar*\n\n*• Ejemplo:*\n- ${usedPrefix + command}emojimix 🐱+👻\n\n[ mínimo 2 emojis ]`
   let [emoji1, emoji2] = text.split`+`
   const anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)).json()
   if (anu.results[0] == undefined) throw 'No se encontró la combinación de los emojis'
