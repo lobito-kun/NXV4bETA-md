4a03027ab884028ffd33fc75f8366d7081f8e9d2
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-20 17:46:46 -0500

    Update config.js

--- config.js
+++ config.js
@@ -52,6 +52,20 @@ global.imgmenu = fs.readFileSync('./storage/image/menu.jpg')
 global.miniurl = fs.readFileSync('./storage/image/miniurl.jpg')
 
 
+global.stime = function (seconds) {
+  seconds = Number(seconds);
+  var d = Math.floor(seconds / (3600 * 24));
+  var h = Math.floor((seconds % (3600 * 24)) / 3600);
+  var m = Math.floor((seconds % 3600) / 60);
+  var s = Math.floor(seconds % 60);
+  var dDisplay = d > 0 ? d + (d == 1 ? " dia, " : " Dias, ") : "";
+  var hDisplay = h > 0 ? h + (h == 1 ? " hora, " : " Horas, ") : "";
+  var mDisplay = m > 0 ? m + (m == 1 ? " minuto, " : " Minutos, ") : "";
+  var sDisplay = s > 0 ? s + (s == 1 ? " segundo" : " Segundos") : "";
+  return dDisplay + hDisplay + mDisplay + sDisplay;
+};
+
+
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
   unwatchFile(file)
