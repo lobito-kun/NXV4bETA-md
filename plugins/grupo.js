b19f32915c9c093fb79dd59d7e5db7dffabc7847
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-20 17:24:43 -0500

    Update and rename group-settings.js to grupo.js

--- plugins/grupo.js
+++ plugins/grupo.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    let isClose = { // Switch Case Like :v
+  let isClose = { // Switch Case Like :v
         'open': 'not_announcement',
         'close': 'announcement',
     }[(args[0] || '')]
@@ -9,11 +9,12 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   *▢ ${usedPrefix + command} close*
   *▢ ${usedPrefix + command} open*
 `.trim()
-    await conn.groupSettingUpdate(m.chat, isClose)
+  await conn.groupSettingUpdate(m.chat, isClose)
 }
-handler.help = ['group *open / close*']
+
+handler.help = ['grupo']
 handler.tags = ['group']
-handler.command = ['group', 'grupo'] 
+handler.command = /^(grupo|group|grup|gp)$/i
 
 handler.admin = true
 handler.botAdmin = true
