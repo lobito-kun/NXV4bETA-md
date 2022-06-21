99423cb2a2df567abf1e753fc5cbc57a4e6834a5
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-20 20:41:37 -0500

    Update rpg-pescar.js

--- plugins/rpg-pescar.js
+++ plugins/rpg-pescar.js
@@ -1,4 +1,5 @@
-let handler = async (m, { conn, usedPrefix }) => {
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `Elige un lugar de pesca`
   let user = global.db.data.users[m.sender]
   let rod = global.db.data.users[m.sender].rod
   let fdurability = global.db.data.users[m.sender].roddurability
@@ -12,7 +13,8 @@ let handler = async (m, { conn, usedPrefix }) => {
   let durability = (Math.floor(Math.random() * (50 - 100) + 100) + 1)
   let ca = (rod == 0 ? 'No tiene' : '' || rod == 1 ? 'normal' : '' || rod == 2 ? 'Caña ????' : '' || rod == 3 ? 'Caña ????' : '' || rod == 4 ? 'Caña ????' : '' || rod == 5 ? 'Caña ????' : '')
   let word = (pickRandom(['un rio 🚣‍♀', 'el mar ⛵', 'en una playa 🏖️']))
-    
+
+  if (text.includes('mar profundo')) {
   if (rod > 0) {
   if (global.db.data.users[m.sender].roddurability > 99) {
   if (new Date - global.db.data.users[m.sender].lastfishing > 180000) {
@@ -24,7 +26,7 @@ let handler = async (m, { conn, usedPrefix }) => {
   global.db.data.users[m.sender].roddurability -= durability * 1
   global.db.data.users[m.sender].exp += exp * 1
   let teks = `
-*Pescaste en ${word} y obtienes*
+*Pescaste en el mar profundo ⛵ y obtienes*
 
 *🎣 Caña:* ${ca}
 
@@ -37,9 +39,10 @@ let handler = async (m, { conn, usedPrefix }) => {
 `.trim()
   conn.reply(m.chat, teks, m)
   
-      } else m.reply(`Te quedaste sin energía vuelve dentro de *${waktur}*`)
-    } else m.reply(`Repara tu caña de pescar, escribiendo ${usedPrefix}reparar caña`)
-  } else m.reply(`Todavía no tienes una caña de pescar, compralo escribiendo ${usedPrefix}comprar caña`)
+        } else m.reply(`Te quedaste sin energía vuelve dentro de *${waktur}*`)
+      } else m.reply(`Repara tu caña de pescar, escribiendo ${usedPrefix}reparar caña`)
+    } else m.reply(`Todavía no tienes una caña de pescar, compralo escribiendo ${usedPrefix}comprar caña`)
+  }
 }
 
 handler.help = ['pescar']
