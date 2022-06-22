40d9481ee8a451c2600e9505f24178d934593858
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-22 16:28:33 -0500

    Update Test.js

--- plugins/Test.js
+++ plugins/Test.js
@@ -41,7 +41,7 @@ contextInfo: {
 "previewType": "VIDEO",
 "thumbnail": miniurl,
 "mediaUrl": 'https://chat.whatsapp.com/ELn6Ck7InoP6UmA3QiQsgo',
-"sourceUrl": 'https://chat.whatsapp.com/ELn6Ck7InoP6UmA3QiQsgo' }}} 
+"sourceUrl": 'https://github.com' }}} 
 conn.sendMessage(m.chat, buttonMessage, { quoted: m })
 
 }
