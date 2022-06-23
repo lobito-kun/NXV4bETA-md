0585bc479543062300d8710262b418efec0efeff
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-23 16:55:37 -0500

    Update wikipedia.js

--- plugins/wikipedia.js
+++ plugins/wikipedia.js
@@ -1,26 +1,24 @@
 import axios from 'axios'
 import cheerio from 'cheerio'
+import fs from 'fs'
 
-
-let handler = async (m, { text }) => {
-    try {
-    //  if (!text) throw `✳️ Ingrese lo que quiere buscar en Wikipedia`
-	const link =  await axios.get(`https://es.wikipedia.org/wiki/${text}`)
-	const $ = cheerio.load(link.data)
-	let wik = $('#firstHeading').text().trim()
-	let resulw = $('#mw-content-text > div.mw-parser-output').find('p').text().trim()
-	m.reply(`▢ *Wikipedia*
-
-‣ Buscado : ${wik}
-
-${resulw}`)
+let handler = async (m, { text, usedPrefix, command }) => {
+  try {
+  if (!text) throw `*⛌ Ingrese lo que quiere buscar en Wikipedia*\n\n*• Ejemplo:*\n- ${usedPrefix + command} minecraft`
+  conn.reply(m.chat, global.wait, m) 
+  const link =  await axios.get(`https://es.wikipedia.org/wiki/${text}`)
+  const $ = cheerio.load(link.data)
+  let wik = $('#firstHeading').text().trim()
+  let resulw = $('#mw-content-text > div.mw-parser-output').find('p').text().trim()
+  let imgwiki = fs.readFileSync('./storage/image/npm.jpg')
+  await conn.sendFile(m.chat, imgwiki, 'Error.jpg', `*• Buscado:* ${wik}\n\n${resulw}`, m)
 } catch (e) {
-  m.reply('⚠️ No se han encontrado resultados ')
+  m.reply('No se han encontrado resultados')
 }
 }
+
 handler.help = ['wikipedia']
 handler.tags = ['tools']
-handler.command = ['wiki','wikipedia'] 
-
+handler.command = /^(wiki|wikipedia)$/i
 
 export default handler
