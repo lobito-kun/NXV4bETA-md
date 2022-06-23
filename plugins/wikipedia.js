d4a93bfe73bf62f361a46d08a1fce20c2827cd11
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-23 16:57:50 -0500

    Update wikipedia.js

--- plugins/wikipedia.js
+++ plugins/wikipedia.js
@@ -3,18 +3,19 @@ import cheerio from 'cheerio'
 import fs from 'fs'
 
 let handler = async (m, { text, usedPrefix, command }) => {
-  try {
+
   if (!text) throw `*⛌ Ingrese lo que quiere buscar en Wikipedia*\n\n*• Ejemplo:*\n- ${usedPrefix + command} minecraft`
-  conn.reply(m.chat, global.wait, m) 
+  conn.reply(m.chat, global.wait, m)
+  try {
   const link =  await axios.get(`https://es.wikipedia.org/wiki/${text}`)
   const $ = cheerio.load(link.data)
   let wik = $('#firstHeading').text().trim()
   let resulw = $('#mw-content-text > div.mw-parser-output').find('p').text().trim()
   let imgwiki = fs.readFileSync('./storage/image/wikipedia.jpg')
   await conn.sendFile(m.chat, imgwiki, 'Error.jpg', `*• Buscado:* ${wik}\n\n${resulw}`, m)
-} catch (e) {
+  } catch (e) {
   m.reply('No se han encontrado resultados')
-}
+  }
 }
 
 handler.help = ['wikipedia']
