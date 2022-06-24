3dd94f9bb969593d43854cfef72fd430723702d1
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-23 19:43:43 -0500

    Update rpg-reclamar.js

--- plugins/rpg-reclamar.js
+++ plugins/rpg-reclamar.js
@@ -1,33 +1,27 @@
 const free = 1000
 const prem = 2000
 
-let handler = async (m, { isPrems }) => {
+const cooldown = 86400000
+
+let handler = async (m, { conn, usedPrefix, command }) => {
   let user = global.db.data.users[m.sender]
-  let time = user.lastclaim + 86400000
-  if (new Date - user.lastclaim < 86400000) throw `Espere *${stime(time - new Date())}* para volver a reclamar`
-  user.money += isPrems ? prem : free
-  user.lastclaim = new Date * 1
-  user.common += 1
-  m.reply(`Has reclamado *$${isPrems ? prem : free}* y una caja *comun*`)
+  let __timers = (new Date - user.lastclaim)
+  let _timers = (cooldown - __timers)
+  let timers = stime(_timers)
+  if (new Date - user.lastclaim > cooldown) {
+      user.money += isPrems ? prem : free
+      user.common += 1
+      user.lastclaim = new Date * 1
+      m.reply(`Has reclamado *$${isPrems ? prem : free}* y una caja *comun*`)
+  } else {
+      m.reply(`Espere *${timers}* para volver a reclamar')
+  }
 }
 
 handler.help = ['reclamar']
-handler.tags = ['xp']
-handler.command = /^(daily|claim|reclamar)$/i
+handler.tags = ['rpg']
+handler.command = /^(reclamar|claim|daily)$/i
+
+handler.cooldown = cooldown
 
 export default handler
-
-
-/*function msToTime(duration) {
-  var milliseconds = parseInt((duration % 1000) / 100),
-    seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
-
-  hours = (hours < 10) ? "0" + hours : hours
-  minutes = (minutes < 10) ? "0" + minutes : minutes
-  seconds = (seconds < 10) ? "0" + seconds : seconds
-
-  return hours + " Horas " + minutes + " Minutos"
-}*/
-
