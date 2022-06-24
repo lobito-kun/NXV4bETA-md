ddf43dc2de755ae8c39b4b3ad9e79b13a3e0ca98
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-24 18:22:14 -0500

    Update sticker-attp.js

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `*⛌ Ingrese el texto que quiera convertir a sticker*\n\n*• Ejemplo:*\n- ${usedPrefix + command} loli`
   let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-  conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
+  conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: teks }), 'Error.webp', '', m, false, { asSticker: true })
 }
 
 handler.help = ['attp']
