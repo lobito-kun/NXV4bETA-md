3a6d0a5728f5e790af4cde679b7db3dd54111d54
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-24 21:18:46 -0500

    Update pinterest.js

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `*⛌ Ingrese el imagen que quiere buscar en Pinterest*\n\n*• Ejemplo:*\n- ${usedPrefix + command} minecraft`
   await conn.reply(m.chat, global.wait, m)
   const json = await pinterest(text)
-  conn.sendFile(m.chat, json.getRandom(), 'Error.jpg', `*• Pinterest:*  ${text}`, m)
+  conn.sendFile(m.chat, json.getRandom(), 'Error.jpg', `*Pinterest:* ${text}`, m)
 }
 
 handler.help = ['pinterest']
