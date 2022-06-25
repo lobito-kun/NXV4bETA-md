fecd1e88fe7d8e83d465419bfa2af9b01ca518ad
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-24 21:17:03 -0500

    Update and rename image-pinterest.js to pinterest.js

---
+++ plugins/pinterest.js
@@ -0,0 +1,14 @@
+import { pinterest } from '@bochilteam/scraper'
+
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `*⛌ Ingrese el imagen que quiere buscar en Pinterest*\n\n*• Ejemplo:*\n- ${usedPrefix + command} minecraft`
+  await conn.reply(m.chat, global.wait, m)
+  const json = await pinterest(text)
+  conn.sendFile(m.chat, json.getRandom(), 'Error.jpg', `*• Pinterest:*  ${text}`, m)
+}
+
+handler.help = ['pinterest']
+handler.tags = ['img']
+handler.command = /^(pinterest)$/i
+
+export default handler
