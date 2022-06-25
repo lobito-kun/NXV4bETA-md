26e33b5989916bc853b321d42625feb7c6473325
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-24 20:50:26 -0500

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -2,7 +2,7 @@ let handler = async (m, { text }) => {
   let user = global.db.data.users[m.sender]
   user.afk = + new Date
   user.afkReason = text
-  m.reply(`\t\t*😴 Ahora estas afk*\n• Usuario: ${conn.getName(m.sender)}\n• Razon: ${text ? ': ' + text : '×'}`)
+  m.reply(`\t\t*😴 Ahora estas afk*\n\n• Usuario: ${conn.getName(m.sender)}\n• Razon: ${text ? ': ' + text : '×'}`)
 }
 
 handler.help = ['afk']
