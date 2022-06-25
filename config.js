dc6500ecd7d8139b6cfded6319e0f7b690065e89
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-24 20:25:15 -0500

    Update config.js

--- config.js
+++ config.js
@@ -57,11 +57,12 @@ global.stime = function clockString(seconds) {
   var h = Math.floor((seconds / (1000 * 60 * 60)) % 24);
   var m = Math.floor((seconds / (1000 * 60)) % 60);
   var s = Math.floor((seconds / 1000) % 60);
-  var dDisplay = d > 0 ? d + (d == 0 ? " dia," : " Dias,") : "";
-  var hDisplay = h > 0 ? + h + (h == 0 ? " hora, " : " Horas, ") : "";
-  var mDisplay = m > 0 ? + m + (m == 0 ? " minuto, " : " Minutos, ") : "";
-  var sDisplay = s > 0 ? + s + (s == 0 ? " segundo" : " Segundos") : "";
-  return dDisplay + hDisplay + mDisplay + sDisplay;
+  var dDisplay = d > 0 ? d + (d == 1 ? " dia," : " Dias,") : "";
+  var hDisplay = h > 0 ? h + (h == 1 ? " hora, " : " Horas, ") : "";
+  var mDisplay = m > 0 ? m + (m == 1 ? " minuto, " : " Minutos, ") : "";
+  var sDisplay = s > 0 ? s + (s == 1 ? " segundo" : " Segundos") : "";
+  return dDisplay + hDisplay + mDisplay + sDisplay ? dDisplay + hDisplay + mDisplay + sDisplay : '0 segundos'
+  //return dDisplay + hDisplay + mDisplay + sDisplay;
 };
 
 
