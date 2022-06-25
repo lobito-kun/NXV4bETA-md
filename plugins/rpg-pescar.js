6d8ef7b91452316a5d0a8005266073a623644e2c
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-25 14:35:38 -0500

    Update rpg-pescar.js

--- plugins/rpg-pescar.js
+++ plugins/rpg-pescar.js
@@ -6,9 +6,14 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
   let __waktur = (new Date - global.db.data.users[m.sender].lastfishing)
   let _waktur = (180000 - __waktur)
   let waktur = stime(_waktur)
-  let blowfish = (rod == 1 ? Math.floor(Math.random() * (8 - 10) + 10) + 1 : '')
+  let commonfish = (rod == 1 ? Math.floor(Math.random() * (35 - 40) + 40) + 1 : '')
   let tropicalfish = (rod == 1 ? Math.floor(Math.random() * (11 - 13) + 13) + 1 : '')
-  let commonfish = (rod == 1 ? Math.floor(Math.random() * (50 - 60) + 60) + 1 : '')
+  let blowfish = (rod == 1 ? Math.floor(Math.random() * (8 - 10) + 10) + 1 : '')
+  let crab = (rod == 1 ? Math.floor(Math.random() * (15 - 20) + 20) + 1 : '')
+  let locust = (rod == 1 ? Math.floor(Math.random() * (6 - 8) + 8) + 1 : '')
+  let shrimp = (rod == 1 ? Math.floor(Math.random() * (5 - 7) + 7) + 1 : '')
+  let squid = (rod == 1 ? Math.floor(Math.random() * (4 - 6) + 6) + 1 : '')
+  let octopus = (rod == 1 ? Math.floor(Math.random() * (3 - 5) + 5) + 1 : '')
   let exp = (rod == 1 ? Math.floor(Math.random() * (300 - 400) + 400) + 1 : '')
   let durability = (Math.floor(Math.random() * (50 - 100) + 100) + 1)
   let ca = (rod == 0 ? 'no tiene' : '' || rod == 1 ? 'normal' : '' || rod == 2 ? 'antigua' : '' || rod == 3 ? 'corupta' : '' )
@@ -27,7 +32,7 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
   case '1':
     if (rod > 0) {
     if (user.roddurability > 99) {
-    if (user.level <= 5) throw 'Minimo nivel 5 para pescar en esta zona'
+    if (user.level <= 4) throw 'Minimo nivel 5 para pescar en esta zona'
     if (new Date - user.lastfishing > 180000) {
         user.lastfishing = new Date * 1
         user.roddurability -= durability * 1
@@ -57,7 +62,7 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
   case '2':
     if (rod > 0) {
     if (user.roddurability > 99) {
-    if (user.level <= 10) throw 'Minimo nivel 10 para pescar en esta zona'
+    if (user.level <= 9) throw 'Minimo nivel 10 para pescar en esta zona'
     if (new Date - user.lastfishing > 180000) {
         user.lastfishing = new Date * 1 
         user.roddurability -= durability * 1 
@@ -73,7 +78,7 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 *Obtienes:*
 ◦ Pez comun: ${blowfish}
 ◦ Pez tropical: ${tropicalfish}
-◦ Cangrejo: ${commonfish}
+◦ Cangrejo: ${crab}
 ◦ Exp: ${exp}
 
 *Durabilidad:* [ *-${durability}* ]
@@ -86,15 +91,15 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 
   case '3':
     if (rod > 0) {
-    if (user.level <= 20) throw 'Minimo nivel 20 para pescar en esta zona'
+    if (user.level <= 19) throw 'Minimo nivel 20 para pescar en esta zona'
     if (user.roddurability > 99) {
     if (new Date - user.lastfishing > 180000) {
-        user.lastfishing = new Date * 1
-        user.blowfish += blowfish * 1 
-        user.tropicalfish += tropicalfish * 1 
-        user.commonfish += commonfish * 1 
+        user.lastfishing = new Date * 1 
         user.roddurability -= durability * 1
-        user.exp += exp * 1
+        user.commonfish += commonfish * 1 
+        user.crab += crab * 1 
+        user.locust += locust * 1 
+        user.exp += exp * 1 
     let teks = `
 *Pescaste en ${mr.m3}*
 
@@ -102,8 +107,8 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 
 *Obtienes:*
 ◦ Pez comun: ${blowfish}
-◦ Cangrejo: ${tropicalfish}
-◦ Langosta: ${commonfish}
+◦ Cangrejo: ${crab}
+◦ Langosta: ${locust}
 ◦ Exp: ${exp}
 
 *Durabilidad:* [ *-${durability}* ]
@@ -117,23 +122,23 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
   case '4':
     if (rod > 0) {
     if (user.roddurability > 99) {
-    if (user.level <= 30) throw 'Minimo nivel 30 para pescar en esta zona'
+    if (user.level <= 29) throw 'Minimo nivel 30 para pescar en esta zona'
     if (new Date - user.lastfishing > 180000) {
-        user.lastfishing = new Date * 1
-        user.blowfish += blowfish * 1 
-        user.tropicalfish += tropicalfish * 1 
-        user.commonfish += commonfish * 1 
+        user.lastfishing = new Date * 1 
         user.roddurability -= durability * 1
-        user.exp += exp * 1
+        user.crab += crab * 1 
+        user.locust += locust * 1 
+        user.shrimp += shrimp * 1 
+        user.exp += exp * 1 
     let teks = `
 *Pescaste en ${mr.m4}*
 
 *🎣 Caña:* ${ca}
 
 *Obtienes:*
-◦ Cangrejo: ${blowfish}
-◦ Langosta: ${tropicalfish}
-◦ Camaron: ${commonfish}
+◦ Cangrejo: ${crab}
+◦ Langosta: ${locust}
+◦ Camaron: ${shrimp}
 ◦ Exp: ${exp}
 
 *Durabilidad:* [ *-${durability}* ]
@@ -147,23 +152,23 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
   case '5':
     if (rod > 0) {
     if (user.roddurability > 99) {
-    if (user.level <= 40) throw 'Minimo nivel 40 para pescar en esta zona'
+    if (user.level <= 39) throw 'Minimo nivel 40 para pescar en esta zona'
     if (new Date - user.lastfishing > 180000) {
-        user.lastfishing = new Date * 1
-        user.blowfish += blowfish * 1 
-        user.tropicalfish += tropicalfish * 1 
-        user.commonfish += commonfish * 1 
+        user.lastfishing = new Date * 1 
         user.roddurability -= durability * 1
-        user.exp += exp * 1
+        user.commonfish += commonfish * 1 
+        user.squid += squid * 1 
+        user.octopus += octopus * 1 
+        user.exp += exp * 1 
     let teks = `
 *Pescaste en ${mr.m5}*
 
 *🎣 Caña:* ${ca}
 
 *Obtienes:*
-◦ Pez comun: ${blowfish}
-◦ Calamar: ${tropicalfish}
-◦ Pulpo: ${commonfish}
+◦ Pez comun: ${commonfish}
+◦ Calamar: ${squid}
+◦ Pulpo: ${octopus}
 ◦ Exp: ${exp}
 
 *Durabilidad:* [ *-${durability}* ]
