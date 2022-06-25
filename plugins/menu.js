3313380e8fc0085fedf4f125b1f3b957e0f186c3
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-25 10:42:39 -0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -60,7 +60,7 @@ Lolita bot remastered by *%wasp*
 
 \t\t\`\`\`L I S T A  -  M E N U S\`\`\`
 `.trimStart(),
-  header: '➜ ᎒⠈- *%category*',
+  header: '➜ ꒰ ❛⠈- *%category*',
   body: '₊❏❜ ⋮  *%cmd*\n',
   footer: '╶',
   after: '',
