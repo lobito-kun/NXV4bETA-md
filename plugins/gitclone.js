0fca7ec15636a2a30fbf4f56baebbd0be7a2b3a0
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-25 19:34:43 -0500

    Update gitclone.js

--- plugins/gitclone.js
+++ plugins/gitclone.js
@@ -1,14 +1,14 @@
 import fetch from 'node-fetch'
+
 const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
 
 let handler = async (m, { args, usedPrefix, command }) => {
-    if (!args[0]) throw `¿Dónde está el enlace de github?\n\n*• Ejemplo:* ${usedPrefix + command} https://github.com/FG98F/dylux-fg`
-    if (!regex.test(args[0])) throw 'link incorrecto'
+    if (!args[0]) throw `*⛌ Ingrese el link de un repositorio de github que desea descargar*\n\n*• Ejemplo:*\n- ${usedPrefix + command} https://github.com/g4tito/lolibot-md`
+    if (!regex.test(args[0])) throw 'El link es incorrecto'
     let [_, user, repo] = args[0].match(regex) || []
     repo = repo.replace(/.git$/, '')
     let url = `https://api.github.com/repos/${user}/${repo}/zipball`
     let filename = (await fetch(url, { method: 'HEAD' })).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
-  
     m.reply(`*↓ ‧ Descargando repositorio, espere . . .*`)
     conn.sendFile(m.chat, url, filename, null, m)
 }
