a385921abcdce6a30dbda58fe70e73313334a854
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-29 19:42:49 -0500

    Update rpg-minar.js

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -12,7 +12,7 @@ let handler = async (m, { conn, text, usedPrefix, command, args, isOwner, isAdmi
     let tin = Math.floor(Math.random() * (25 - 30) + 30) + 1
     let exp = tin * 10
     user.mineral_tin += tin * 1 
-    user.mineral_tin += exp * 1 
+    user.exp += exp * 1 
     let m1 = `
 *Minaste 🏔️ mineral de estaño*
 
@@ -23,9 +23,65 @@ let handler = async (m, { conn, text, usedPrefix, command, args, isOwner, isAdmi
 *⛏️ Pico:* normal
 `.trim()
     m.reply(m1)
-    user.lastmiming = new Date * 1
+    user.lastmiming = new Date * 1 
   break
 
+  case 'cobre':
+  case 'copper':
+    let copper = Math.floor(Math.random() * (25 - 30) + 30) + 1
+    let exp = copper * 10
+    user.mineral_copper += copper * 1 
+    user.exp += exp * 1 
+    let m2 = `
+*Minaste 🏔️ mineral de cobre*
+
+*Obtienes:*
+◦ Cobre: ${copper}
+◦ Exp: ${exp}
+
+*⛏️ Pico:* normal
+`.trim()
+    m.reply(m2)
+    user.lastmiming = new Date * 1 
+  break
+
+  case 'hierro':
+  case 'iron':
+    let iron = Math.floor(Math.random() * (25 - 30) + 30) + 1
+    let exp = iron * 10
+    user.mineral_iron += iron * 1 
+    user.exp += exp * 1 
+    let m3 = `
+*Minaste 🏔️ mineral de hierro*
+
+*Obtienes:*
+◦ Hierro: ${iron}
+◦ Exp: ${exp}
+
+*⛏️ Pico:* normal
+`.trim()
+    m.reply(m3)
+    user.lastmiming = new Date * 1 
+  break
+
+  case 'sal':
+  case 'salt':
+    let salt = Math.floor(Math.random() * (25 - 30) + 30) + 1
+    let exp = salt * 10
+    user.mineral_salt += salt * 1 
+    user.exp += exp * 1 
+    let m4 = `
+*Minaste 🏔️ mineral de sal*
+
+*Obtienes:*
+◦ Sal: ${salt}
+◦ Exp: ${exp}
+
+*⛏️ Pico:* normal
+`.trim()
+    m.reply(m4)
+    user.lastmiming = new Date * 1 
+  break
   }
 }
 
