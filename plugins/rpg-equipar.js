abf65b42b29cf86d7094729f72c5b11d4b4f80b3
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-30 17:13:25 -0500

    Update rpg-equipar.js

--- plugins/rpg-equipar.js
+++ plugins/rpg-equipar.js
@@ -1,1 +1,20 @@
-//Pronto...
+let handler = async (m, { conn, text, usedPrefix, command, args, isOwner, isAdmin, isROwner, isPrems }) => {
+
+  let user = global.db.data.users[m.sender]
+  let min = user.pickaxe_bronze + user.pickaxe_iron + user.pickaxe_steel + user.pickaxe_crimsteel + user.pickaxe_mythan + user.pickaxe_cobalt + user.pickaxe_varaxite + user.pickaxe_magic + user.pickaxe_umbral + user.pickaxe_ancient
+
+  let type = (args[0] || ' ').toLowerCase()
+
+  switch (type) {
+  case 'pico':
+    if (min == 0) throw 'No tienes ningun pico para equiparte'
+    m.reply(`Te equipaste un pico de ${pick}`)
+  break
+  }
+}
+
+handler.help = ['equipar']
+handler.tags = ['rpg']
+handler.command = /^(equipar)$/i
+
+export default handler
