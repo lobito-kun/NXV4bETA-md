6df4740a5b6684d8c28c6e099cc76dc394da8374
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-06-29 21:02:55 -0500

    Update rpg-minar.js

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -2,8 +2,8 @@ let handler = async (m, { conn, text, usedPrefix, command, args, isOwner, isAdmi
 
   let user = global.db.data.users[m.sender]
   let min = `${user.pickaxe_bronze + user.pickaxe_iron + user.pickaxe_steel + user.pickaxe_crimsteel + user.pickaxe_mythan + user.pickaxe_cobalt + user.pickaxe_varaxite + user.pickaxe_magic + user.pickaxe_umbral + user.pickaxe_ancient}`
-  m.reply(`${min}`)
-  if (min < 0) throw 'Necesitas un pico para extraer este mineral'
+  let minn = `${min}`
+  if (minn < 0) throw 'Necesitas un pico para extraer este mineral'
   let time = user.lastmiming + 37500 
   if (new Date - user.lastmiming < 37500) throw `Espera *${stime(time - new Date())}* para volver a minar` 
 
