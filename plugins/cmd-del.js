8f8e070a0b67ba71a795ceb743a912765243ec6a
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-07-03 19:46:12 -0500

    Update cmd-del.js

--- plugins/cmd-del.js
+++ plugins/cmd-del.js
@@ -1,16 +1,15 @@
 let handler = async (m, { conn, usedPrefix, text, command }) => {
-    let hash = text
-    if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex')
-    if (!hash) throw `✳️ Ingrese el nombre del comamdo`
-    let sticker = global.db.data.sticker
-    if (sticker[hash] && sticker[hash].locked) throw '✳️ No puedes borrar este comando'
-    delete sticker[hash]
-    m.reply(`✅ Comando eliminado`)
+  let hash = text
+  if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex')
+  if (!hash) throw 'Ingrese el nombre del comando para eliminar'
+  let sticker = global.db.data.sticker
+  if (sticker[hash] && sticker[hash].locked) throw 'No puedes eliminar este comando'
+  delete sticker[hash]
+  m.reply(`Se eliminó el comando [ *${sticker.text}* ]`)
 }
-
 
 handler.help = ['delcmd']
 handler.tags = ['database']
-handler.command = ['delcmd']
+handler.command = /^(delcmd)$/i
 
 export default handler
