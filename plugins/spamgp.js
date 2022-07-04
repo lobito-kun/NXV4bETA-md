24c7f51cf8e6e954b20d5ed5ad77ac2c65f90e71
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-07-04 18:10:43 -0500

    Create spamgp.js

---
+++ plugins/spamgp.js
@@ -0,0 +1,21 @@
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
+
+let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
+  if (!text) throw `Ingrese el link de un grupo de WhatsApp`
+  let [_, code, expired] = text.match(linkRegex) || []
+  if (!code) throw 'El link es invalido'
+  let res = await conn.groupAcceptInvite(code)
+  await m.reply(`${JSON.stringify(res, null, 1)}`)
+  await m.reply(`*Enviando spam . . .*`)
+}
+
+handler.help = ['spamjp']
+handler.tags = ['premium']
+
+handler.command = ['spamgp'] 
+
+handler.owner = true
+
+export default handler
+
+const isNumber = (x) => (x = parseInt(x), typeof x === 'number' && !isNaN(x))*/
