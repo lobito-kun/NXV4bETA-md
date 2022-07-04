4dec8e24bafe0ea5b4e5e8370b4a1a33e3e659d3
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-07-04 00:58:39 -0500

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -6,11 +6,11 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
     if (m.isBaileys && m.fromMe)
         return !0
     if (!m.isGroup) return !1
-    let chat = db.data.chats[m.chat]
+    let gchat = db.data.chats[m.chat]
     let bot = db.data.settings[this.user.jid] || {}
     const isGroupLink = linkRegex.exec(m.text)
 
-    if (chat.antiLink && isGroupLink && !isAdmin) {
+    if (gchat.antiLink && isGroupLink && !isAdmin) {
         if (isBotAdmin) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
             if (m.text.includes(linkThisGroup)) return !0
@@ -19,9 +19,9 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
             
 No permitimos enlaces de otros grupos 
 lo siento *${await this.getName(m.sender)}*  serás expulsado del grupo ${isBotAdmin ? '' : '\n\nNo soy admin así que no te puedo expulsar :"v'}`, igfg, ['Desactivar AntiLink', '/off antilink'], m)
-        if (isBotAdmin && chat.antiLink) {
+        if (isBotAdmin && gchat.antiLink) {
             await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-        } else if (!chat.antiLink) return //m.reply('')
+        } else if (!gchat.antiLink) return //m.reply('')
     }
     return !0
 }
