4d586a368625d882471401c51873d1df88ae7e20
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-07-04 00:54:09 -0500

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,27 +1,27 @@
-let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+import db from '../lib/database.js'
 
-export async function before(m, { isAdmin, isBotAdmin }) {
-  if (m.isBaileys && m.fromMe) return !0
-  if (!m.isGroup) return !1
-  let chat = global.db.data.chats[m.chat]
-  let bot = global.db.data.settings[this.user.jid] || {}
-  const isGroupLink = linkRegex.exec(m.text)
+const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
 
-  if (chat.antiLink && isGroupLink && !isAdmin) {
-      if (isBotAdmin) {
-          const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-          if (m.text.includes(linkThisGroup)) return !0
-      }
-  await conn.reply(`\t\t\`\`\`「 Enlace Detectado 」\`\`\`
+export async function before(m, {conn, isAdmin, isBotAdmin }) {
+    if (m.isBaileys && m.fromMe)
+        return !0
+    if (!m.isGroup) return !1
+    let chat = db.data.chats[m.chat]
+    let bot = db.data.settings[this.user.jid] || {}
+    const isGroupLink = linkRegex.exec(m.text)
 
-En este grupo no esta permitido enviar enlaces de otros grupos por lo tanto serás baneado`)
-/*conn.sendButton(m.chat, `*≡ *
+    if (chat.antiLink && isGroupLink && !isAdmin) {
+        if (isBotAdmin) {
+            const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
+            if (m.text.includes(linkThisGroup)) return !0
+        }
+        await conn.sendButton(m.chat, `*≡ Enlace Detectado*
             
 No permitimos enlaces de otros grupos 
-lo siento *${await this.getName(m.sender)}*  serás expulsado del grupo ${isBotAdmin ? '' : '\n\nNo soy admin así que no te puedo expulsar :"v'}`, igfg, ['Desactivar AntiLink', '/off antilink'], m)*/
-      if (isBotAdmin && chat.antiLink) {
-          await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-      } else if (!chat.antiLink) return //m.reply('')
-   }
-  return !0
+lo siento *${await this.getName(m.sender)}*  serás expulsado del grupo ${isBotAdmin ? '' : '\n\nNo soy admin así que no te puedo expulsar :"v'}`, igfg, ['Desactivar AntiLink', '/off antilink'], m)
+        if (isBotAdmin && chat.antiLink) {
+            await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+        } else if (!chat.antiLink) return //m.reply('')
+    }
+    return !0
 }
