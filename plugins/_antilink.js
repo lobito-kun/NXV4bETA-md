7b8fe5fb341d807a7cd6092c07f94119e8f01bad
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-07-04 01:26:03 -0500

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,23 +1,24 @@
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+import db from '../lib/database.js'
 
-export async function before(m, { isAdmin, isBotAdmin }) {
-if (m.isBaileys && m.fromMe)
-return !0
-if (!m.isGroup) return !1
-let chat = global.db.data.chats[m.chat]
-let bot = global.db.data.settings[this.user.jid] || {}
-const isGroupLink = linkRegex.exec(m.text)
-const grupo = `https://chat.whatsapp.com`
-if (isAdmin && chat.antiLink && m.text.includes(grupo)) return !1
-if (chat.antiLink && isGroupLink && !isAdmin) {
-if (isBotAdmin) {
-const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-if (m.text.includes(linkThisGroup)) return !0
-}    
-await conn.reply(`\t\t\`\`\`「 Enlace Detectado 」\`\`\`\n\nEn este grupo no esta permitido enviar enlaces de otros grupos por lo tanto serás baneado`)
-if (isBotAdmin) {
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-}
-}
-return !0
+const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+
+export async function before(m, {conn, isAdmin, isBotAdmin }) {
+    if (m.isBaileys && m.fromMe)
+        return !0
+    if (!m.isGroup) return !1
+    let chat = db.data.chats[m.chat]
+    let bot = db.data.settings[this.user.jid] || {}
+    const isGroupLink = linkRegex.exec(m.text)
+
+    if (chat.antiLink && isGroupLink && !isAdmin) {
+        if (isBotAdmin) {
+            const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
+            if (m.text.includes(linkThisGroup)) return !0
+        }
+        await m.reply(`\t\t\`\`\`「 Enlace Detectado 」\`\`\`\n\nEn este grupo no esta permitido enviar enlaces de otros grupos por lo tanto serás baneado`)
+        if (isBotAdmin && chat.antiLink) {
+            await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+        } else if (!chat.antiLink) return //m.reply('')
+    }
+    return !0
 }
