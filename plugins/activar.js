3e434f4f8ef7b7fbfabb32302c04373a84aba30b
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-07-10 00:33:56 -0500

    Update activar.js

--- plugins/activar.js
+++ plugins/activar.js
@@ -12,12 +12,12 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
     case 'off':
       if (chat.autosticker) throw m.reply('La función autosticker ya ha sido desactivado')
       chat.autosticker = false 
-      await m.reply('Se desactivó la función de autosticker en este grupo')
+      m.reply('Se desactivó la función de autosticker en este grupo')
     break
     case 'on':
       if (chat.autosticker) throw m.reply('La función autosticker ya ha sido activado')
       chat[m.chat].autosticker = true 
-      await m.reply('Se activó la función de autosticker en este grupo')
+      m.reply('Se activó la función de autosticker en este grupo')
     break
     }
   break
