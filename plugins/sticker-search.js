7073526f7cff296b852b262fac385cd9832179f4
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-07-10 17:32:33 -0500

    Update sticker-search.js

--- plugins/sticker-search.js
+++ plugins/sticker-search.js
@@ -6,8 +6,7 @@ let handler = async (m, { conn, text }) => {
   if (!text) throw `Example : ${prefix + command} loli`
   let anu = await RA.StickerSearch(text)
   for (let i = 0; i < (anu.data.sticker.length < 5 ? anu.data.sticker.length : 5); i++) {
-  await m.reply(anu.data.sticker.length[i])
-  //await conn.sendFile(m.chat, anu.data.sticker.length[i], null, { asSticker: true }, m)
+  await conn.sendFile(m.chat, anu.data.sticker[i], null, { asSticker: true }, m)
   }
 }
 
