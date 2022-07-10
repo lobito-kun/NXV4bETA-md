89c9b243a309bdada20121a30cc4a373e03494ed
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-07-10 12:43:24 -0500

    Update crear-grupo.js

--- plugins/crear-grupo.js
+++ plugins/crear-grupo.js
@@ -4,9 +4,9 @@ let handler = async (m, { conn, text }) => {
   let group = await conn.groupCreate(text, [m.sender])
   let link = await conn.groupInviteCode(group.gid)
   let url = 'https://chat.whatsapp.com/' + link;
-  await m.reply(`\t*✅ Grupo creado correctamente*\n\n• Nombre: ${text}\n• ID: ${group.gid}`)
+  m.reply(`\t*✅ Grupo creado correctamente*\n\n• Nombre: ${text}\n• ID: ${group.gid}`)
   } catch (e) {
-  await m.reply(`Error al crear el grupo`)
+  m.reply(`Error al crear el grupo`)
   }
 }
 
