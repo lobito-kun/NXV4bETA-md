04d7a0122e5572c1593103b9e7fb193e74775a52
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-07-09 23:43:10 -0500

    Update activar-desactivar.js

--- plugins/activar-desactivar.js
+++ plugins/activar-desactivar.js
@@ -1,1 +1,36 @@
-//Test
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+
+  let enn = `-`
+  await conn.reply(m.chat, enn, m)
+
+  let type = (command).toLowerCase()
+  let type2 = (args[0] || ' ').toLowerCase()
+
+  switch (type) {
+  case 'autosticker':
+    await conn.sendButton(m.chat, '', wm, [['Desactivar', `${usedPrefix + command} off`], ['Activar', `${usedPrefix + command} on`]], m)
+    switch (type2) {
+    case 'off':
+      global.db.data.chats[m.chat].autosticker = false 
+      await m.reply('Se desactivó la función de autosticker en este grupo')
+    break
+    case 'on':
+      global.db.data.chats[m.chat].autosticker = true 
+      await m.reply('Se activó la función de autosticker en este grupo')
+    break
+    }
+  break
+  }
+}
+
+handler.help = ['autosticker']
+handler.tags = ['random']
+handler.command = /^(autosticker)$/i
+
+export default handler
+
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
