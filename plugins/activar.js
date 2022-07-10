377b81a1f6d1b8f4a633bd83b9b0ebe1b4d73a57
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-07-10 01:13:57 -0500

    Update activar.js

--- plugins/activar.js
+++ plugins/activar.js
@@ -9,12 +9,13 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
   'activar': true,
   'desactivar': false,
   }[(args[0] || '')]
+  let fr = 'Elija un opción'
 
   switch (type) {
   case 'autosticker':
-    if (!text) throw conn.sendButton(m.chat, '\t\t\t- *Autosticker* - ', '-', [['Desactivar', `${usedPrefix + command} off`], ['Activar', `${usedPrefix + command} on`]], m)
+    if (!text) throw conn.sendButton(m.chat, '*≡ AutoSticker*\n\nConvierte automáticamente los imágenes, gif y videos en sticker\n\nNota :\n[ El video no debe pasar los 10s | 1 MB ]', fr, [['Desactivar', `${usedPrefix + command} off`], ['Activar', `${usedPrefix + command} on`]], m)
     chat.autosticker = isOption
-    await m.reply(`La función autosticker se ${isOption ? 'activó' : 'desactivó'} en este grupo`)
+    await m.reply(`La función AutoSticker se ${isOption ? 'activó' : 'desactivó'} en este Grupo`)
   break
   }
 }
