7b93d8ec22cd8ae7d37cc62a982ddf9df20b12d0
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-07-11 18:03:14 -0500

    Update stickerpack-download.js

--- plugins/stickerpack-download.js
+++ plugins/stickerpack-download.js
@@ -3,6 +3,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `*Ingrese el link de un paquete de stickers*\n-\n*Ejemplo:*\n${usedPrefix + command} https://getstickerpack.com/stickers/minecraft-uwu-boy`
+  await conn.reply(m.chat, '*Enviando los stickers . . .*', m)
   let res = await fetch(`https://api.zacros.my.id/downloader/stickerpack?link=${text}`)
   let json = await res.json()
   for (let data of (json.result || json)) {
