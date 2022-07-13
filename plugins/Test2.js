51d08c44448f4f0b0a07583643913d1119b4ba00
Gɑtito ⾕ <84881966+g4tito@users.noreply.github.com>
2022-07-13 17:28:11 -0500

    Update Test2.js

--- plugins/Test2.js
+++ plugins/Test2.js
@@ -2,7 +2,8 @@ let handler = m => m
 
 handler.before = async function (m) {
   let vnro = ['212', '265', '92', '44', '62', '7', '55', '48', '972', '371', '1', '377']
-  if (m.sender.startsWith(vnro)) {
+  let rnro = vnro.map(v => v.replace(/[^0-9]/g, ''))
+  if (m.sender.startsWith(rnro)) {
   global.db.data.users[m.sender].banned = false 
   m.reply('AntiMoros Activado lo siento mirey seras expulsado del grupo.')
   await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
