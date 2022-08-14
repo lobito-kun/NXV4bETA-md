efeba6e0ada74f956f22e60f84fad68f7fa12be4
⛥TR0Y4NW0LF⛥ <63571538+lobito-kun@users.noreply.github.com>
2022-08-14 16:29:50 -0300

    Update script.js

--- plugins/script.js
+++ plugins/script.js
@@ -1,5 +1,5 @@
 let handler = async function (m, { conn, __dirname }) {
- m.reply(`*Github de este bot:*\nhttps://github.com/g4tito/lolibot-md`)
+ m.reply(`*Este comando esta deseactivado por el programador`)
 }
 
 handler.help = ['script']
